fae746fca54fdb41c155fb0a5617015487ab3adb
Kevin Lenell <krlenell@gmail.com>
2021-05-29 22:45:35 -0700

    added font awesome and made weak header/navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Header></Header>
     <HelloWorld
     msg="So this is how props work eh?"
     class="column is-three-fifths is-offset-one-fifth"
@@ -8,11 +9,13 @@
 </template>
 
 <script>
+import Header from './components/Header.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
+    Header,
     HelloWorld
   }
 }
